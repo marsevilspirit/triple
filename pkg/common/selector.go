cb8284640e490e586bb2395a1d0f1eab053e0c29
marsevilspirit <marsevilspirit@gmail.com>
2025-01-01 01:29:22 +0800

    add mu

--- pkg/common/selector.go
+++ pkg/common/selector.go
@@ -1,16 +1,11 @@
 package common
 
-import (
-	"sync"
-)
-
 type Selector interface {
 	Select(max int) int
 }
 
 type RoundRobinSelector struct {
 	index int
-	mu    sync.Mutex
 }
 
 func NewRoundRobinSelector() *RoundRobinSelector {
@@ -18,9 +13,6 @@ func NewRoundRobinSelector() *RoundRobinSelector {
 }
 
 func (rr *RoundRobinSelector) Select(max int) int {
-	rr.mu.Lock()
-	defer rr.mu.Unlock()
-
 	i := rr.index
 	rr.index = (rr.index + 1) % max
 
